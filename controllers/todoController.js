d33bd73b5522e15376847c6725f90558d1a395e7
James Robert Rooke <thepoltergeist23@gmail.com>
2018-07-04 16:30:10 +0800

    finish todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,45 @@
-moduel.exports = function(app) {
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//connect to database
+mongoose.connect('mongodb://test:testing123@ds127781.mlab.com:27781/james_todos_db', { useNewUrlParser: true });
+
+//create a schema
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+// var data = [
+//   {item: 'get milk'},
+//   {item: 'walk dog'},
+//   {item: 'test this last item'}
+// ];
+
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+
+module.exports = function(app) {
   app.get('/todo', function(request, response) {
-
+    Todo.find({}, function(error, data) {
+      if (error) throw error;
+      response.render('todo', {todos: data});
+    });
   });
 
-  app.post('/todo', function(request, response) {
-
+  app.post('/todo', urlencodedParser, function(request, response) {
+    // create mongodb data
+    var newTodo = Todo(request.body).save(function(error, data) {
+      if (error) throw error;
+      response.json(data);
+    });
   });
 
-  app.delete('/todo', function(request, response) {
-
+  app.delete('/todo/:item', function(request, response) {
+    // delete item from mongodb
+    Todo.find({item: request.params.item.replace(/\-/g," ")}).remove(function(error, data) {
+      if(error) throw error;
+      response.json(data);
+    });
   });
 };
