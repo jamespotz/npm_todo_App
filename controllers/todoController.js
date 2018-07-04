d014a9fb38d39a65c6bcb3efd4bafe2a22c65150
James Robert Rooke <thepoltergeist23@gmail.com>
2018-07-04 13:56:33 +0800

    add todoController

---
+++ controllers/todoController.js
@@ -0,0 +1,13 @@
+moduel.exports = function(app) {
+  app.get('/todo', function(request, response) {
+
+  });
+
+  app.post('/todo', function(request, response) {
+
+  });
+
+  app.delete('/todo', function(request, response) {
+
+  });
+};
